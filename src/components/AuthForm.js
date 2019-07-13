62c0c8d4be06831d8ecd11f6b2e9938892fba556
Dmitriy Kovtun <dkovtun.work@gmail.com>
2019-07-14 00:08:28 +0300

    auth form created and connected to redux

---
+++ src/components/AuthForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import PropTypes from 'prop-types';
+import { authorize } from '../actions/auth';
+
+const AuthForm = props => {
+  const { handleSubmit, pristine, submitting, error } = props;
+  const submit = handleSubmit(authorize);
+
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <label>Authorize</label>
+        <div>
+          <Field name="login" component="input" type="text" placeholder="First Name" />
+        </div>
+      </div>
+      <div>
+        <label>Password</label>
+        <div>
+          <Field name="password" component="input" type="password" placeholder="Password" />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <div>{error}</div>
+      </div>
+    </form>
+  );
+};
+
+AuthForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default reduxForm({
+  form: 'auth',
+})(AuthForm);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
 import auth from './auth';
 
 const rootReducer = combineReducers({
   auth,
+  form: reduxFormReducer,
 });
 
 export default rootReducer;
