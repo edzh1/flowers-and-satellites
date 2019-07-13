0b3b9f4ea611ebb40e09b04032f9799709086691
Dmitriy Kovtun <dkovtun.work@gmail.com>
2019-07-13 20:36:41 +0300

    regular css for components replaced with styled-components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const StyledApp = styled.div`
+  background-color: turquoise;
+`;
+
+function App() {
+  return <StyledApp>Oh, hi Mark!</StyledApp>;
+}
+
+export default App;
